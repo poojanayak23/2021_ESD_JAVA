3ea77634f8c96b019c842e69187391773b8802d7
poojanayak23 <80675089+poojanayak23@users.noreply.github.com>
2021-04-04 21:39:55 +0530

    Add files via upload

---
+++ AgeQ42.java
@@ -0,0 +1,17 @@
+
+class AgeQ42{
+	void age(int i, int s)
+	{
+		int c = i+s-s-12;
+		System.out.println("Charu is 12 years younger than Iqbal");
+		System.out.println("Charu's age is "+c);
+	}
+
+	public static void main(String[] args) {
+		
+		AgeQ42 obj = new AgeQ42();
+		obj.age(14,11);
+	}
+}
+
+}
